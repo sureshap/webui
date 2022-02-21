30c1673d5d8d1577e3d5431aef70e22348a31195
sureshap <81589808+sureshap@users.noreply.github.com>
2022-02-22 00:30:52 +0530

    Update SampleUIApp.py

--- SampleUIApp.py
+++ SampleUIApp.py
@@ -3,3 +3,6 @@ import streamlit as st
 #Headings for Web Application
 st.title("Natural Language Processing Web Application Example")
 st.subheader("What type of NLP service would you like to use?")
+
+#Picking what NLP task you want to do
+option = st.selectbox('NLP Service',('Sentiment Analysis', 'Entity Extraction', 'Text Summarization')) #option is stored in this variable
