4d3d52e1e841e87ae99f72108c79b80044253ff9
sureshap <81589808+sureshap@users.noreply.github.com>
2022-02-21 23:56:35 +0530

    Add files via upload

---
+++ SampleUIApp.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+#Headings for Web Application
+st.title("Natural Language Processing Web Application Example")
+st.subheader("What type of NLP service would you like to use?")
