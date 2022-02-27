eeb50208d4d3f11363cb5118dd7d319ced2be2e7
sureshap <81589808+sureshap@users.noreply.github.com>
2022-02-27 23:58:16 +0530

    Update WebUI2.py

--- WebUI2.py
+++ WebUI2.py
@@ -3,11 +3,8 @@
 Here's our first attempt at using data to create a table:
 """
 
-import streamlit as st
-import pandas as pd
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
 
-df
+st.dataframe(dataframe.style.highlight_max(axis=0))
