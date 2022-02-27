a531e2574104e83559554d8e1d7df938a384af00
sureshap <81589808+sureshap@users.noreply.github.com>
2022-02-28 01:10:24 +0530

    Add files via upload

---
+++ WebUI4.py
@@ -0,0 +1,16 @@
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+# A function to load the data.
+DATE_COLUMN = 'date/time'
+DATA_URL = ('input_data.xlsx')
+
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
