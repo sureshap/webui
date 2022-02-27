3cf5a0b6ec40ea934323336980cbc3c9d48112f9
sureshap <81589808+sureshap@users.noreply.github.com>
2022-02-28 00:07:15 +0530

    Add files via upload

---
+++ WebUI3.py
@@ -0,0 +1,32 @@
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
+
+
+y = st.button('Click') 
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
+
+
+
+if st.checkbox('Show dataframe'):
+    chart_data = pd.DataFrame(
+       np.random.randn(20, 3),
+       columns=['a', 'b', 'c'])
+
+	chart_data
