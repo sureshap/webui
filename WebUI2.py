68f0f59177b5369971f84e5927be9b6285dc1bb2
sureshap <81589808+sureshap@users.noreply.github.com>
2022-02-28 00:01:52 +0530

    Update WebUI2.py

--- WebUI2.py
+++ WebUI2.py
@@ -18,3 +18,9 @@ chart_data = pd.DataFrame(
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
