8adccd44d6ea5f8259d4ece4d626fab5ac80af60
sureshap <81589808+sureshap@users.noreply.github.com>
2022-02-28 02:45:49 +0530

    Add files via upload

---
+++ WebUI5.py
@@ -0,0 +1,25 @@
+
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+# A function to load the data.
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://github.com/sureshap/webui/blob/main/input_data.xlsx')
+
+st.write('Hello')
+
+def load_data(nrows):
+    data = pd.read_excel(DATA_URL, engine='openpyxl')
+    return data
+    
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+#df_data = load_data(8500)
+df_data = pd.read_excel(DATA_URL)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
