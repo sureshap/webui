ea0a3fa68214a6ea03a00820ac6a4be8fcc7b6ea
sureshap <81589808+sureshap@users.noreply.github.com>
2022-02-28 02:38:36 +0530

    Update WebUI4.py

--- WebUI4.py
+++ WebUI4.py
@@ -8,6 +8,6 @@ uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
 
 if uploaded_file:
     df = pd.read_excel(uploaded_file)
-
-    st.dataframe(df.head(10)
-    st.table(df.head(10))
+    df.head(10)
+    #st.dataframe(df.head(10)
+    #st.table(df.head(10))
